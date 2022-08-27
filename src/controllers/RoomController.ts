fa32fd99c90a89483252127178ab9f72fa6743e6
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-26 21:26:55 -0300

    refactoring in room controller

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,96 +1,77 @@
-import { subjectRepository } from "./../repositories/subjectRepository";
 import { Request, Response } from "express";
+import { BadRequestError, NotFoundError } from "../helpers/apiErrors";
 import { roomRepository } from "../repositories/roomRepository";
 import { videoRepository } from "../repositories/videoRepository";
+import { subjectRepository } from "./../repositories/subjectRepository";
 
 export class RoomController {
   async create(req: Request, res: Response) {
     const { name, description } = req.body;
 
     if (!name) {
-      return res.status(400).json({ message: "O nome é obrigatório" });
+      throw new BadRequestError("O nome é obrigatório");
     }
 
-    try {
-      const newRoom = roomRepository.create({ name, description });
+    const newRoom = roomRepository.create({ name, description });
 
-      await roomRepository.save(newRoom);
+    await roomRepository.save(newRoom);
 
-      return res.status(201).json(newRoom);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
+    return res.status(201).json(newRoom);
   }
 
   async createVideo(req: Request, res: Response) {
     const { title, url } = req.body;
     const { idRoom } = req.params;
 
-    try {
-      const room = await roomRepository.findOneBy({ id: Number(idRoom) });
+    const room = await roomRepository.findOneBy({ id: Number(idRoom) });
 
-      if (!room) {
-        return res.status(404).json({ message: "Aula não existe" });
-      }
-
-      const newVideo = videoRepository.create({ title, url, room });
-
-      await videoRepository.save(newVideo);
-
-      return res.status(201).json(newVideo);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Internal Server Error" });
+    if (!room) {
+      throw new NotFoundError("Aula não existe");
     }
+
+    const newVideo = videoRepository.create({ title, url, room });
+
+    await videoRepository.save(newVideo);
+
+    return res.status(201).json(newVideo);
   }
 
   async roomSubject(req: Request, res: Response) {
     const { subject_id } = req.body;
     const { idRoom } = req.params;
 
-    try {
-      const room = await roomRepository.findOneBy({ id: Number(idRoom) });
+    const room = await roomRepository.findOneBy({ id: Number(idRoom) });
 
-      if (!room) {
-        return res.status(404).json({ message: "Aula não existe" });
-      }
-
-      const subject = await subjectRepository.findOneBy({
-        id: Number(subject_id),
-      });
-
-      if (!subject) {
-        return res.status(404).json({ message: "Disciplina não existe" });
-      }
-
-      const roomUpdate = {
-        ...room,
-        subjects: [subject],
-      };
-
-      await roomRepository.save(roomUpdate);
-
-      return res.status(204).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Internal Server Error" });
+    if (!room) {
+      throw new NotFoundError("Aula não existe");
     }
+
+    const subject = await subjectRepository.findOneBy({
+      id: Number(subject_id),
+    });
+
+    if (!subject) {
+      throw new NotFoundError("Disciplina não existe");
+    }
+
+    const roomUpdate = {
+      ...room,
+      subjects: [subject],
+    };
+
+    await roomRepository.save(roomUpdate);
+
+    return res.status(204).send();
   }
 
   async list(req: Request, res: Response) {
-    try {
-      const rooms = await roomRepository.find({
-        relations: {
-          subjects: true,
-          videos: true,
-        },
-      });
+    const rooms = await roomRepository.find({
+      relations: {
+        subjects: true,
+        videos: true,
+      },
+    });
 
-      return res.json(rooms);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
+    return res.json(rooms);
   }
 }
