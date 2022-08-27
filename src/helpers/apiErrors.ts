e378f10c08c6b372be05b2caa5d7c0a6a2cec701
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-08-26 21:17:39 -0300

    add API error classes

---
+++ src/helpers/apiErrors.ts
@@ -0,0 +1,23 @@
+export class ApiError extends Error {
+  constructor(message: string, public readonly statusCode: number) {
+    super(message);
+  }
+}
+
+export class BadRequestError extends ApiError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class NotFoundError extends ApiError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class UnauthorizedError extends ApiError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
